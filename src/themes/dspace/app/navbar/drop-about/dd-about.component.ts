7fb726ade5746889e80f74913751b225f31ef333
AShershun <shershunchik17@gmail.com>
2024-01-03 02:44:10 +0200

    fix DdAboutComponent

---
+++ src/themes/dspace/app/navbar/drop-about/dd-about.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ds-dd-about',
+  styleUrls: ['dd-about.component.scss'],
+  templateUrl: 'dd-about.component.html',
+})
+
+
+export class DdAboutComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+}
